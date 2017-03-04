c51e37c7d6eda7f758148b325e42d3b20359991e
李志伟 <lzwcqx@163.com>
2017-03-04 14:05:14 +0800

    update class name

---
+++ spring-cloud-config-client/src/main/java/com/spring/cloud/config/demo/ConfigClient.java
@@ -0,0 +1,37 @@
+package com.spring.cloud.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@SpringBootApplication
+@RefreshScope
+public class ConfigClient {
+	
+	@Value("${mysqldb.datasource.url}")
+	private String url;
+	@Value("${redis.host}")
+	private String host;
+ 	@Value("${redis.port}")
+	private String port;
+	@RequestMapping(value = "/db")
+	public String db() {
+		return "DB url:" + url;
+	}
+	@RequestMapping(value = "/redis")
+	public String redis() {
+		return "redis.host:" + host + " ,redis.port:" + port;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClient.class, "--server.port=8081");
+	}
+}
