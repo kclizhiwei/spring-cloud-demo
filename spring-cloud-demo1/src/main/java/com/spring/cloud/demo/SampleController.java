238acae7378dc9c7ab7bd1130adc9486646266a2
李志伟 <lzwcqx@163.com>
2017-03-04 13:10:04 +0800

    update name

--- spring-cloud-demo1/src/main/java/com/spring/cloud/demo/SampleController.java
+++ spring-cloud-demo1/src/main/java/com/spring/cloud/demo/SampleController.java
@@ -2,11 +2,15 @@ package com.spring.cloud.demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Configuration
+@EnableAutoConfiguration
 @RestController
 @SpringBootApplication
 @RefreshScope
